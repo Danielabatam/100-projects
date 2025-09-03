45c88ab274397149e121b8502a3915aca6a794c4
Daniela BATAM <danielabatam@gmail.com>
2025-09-03 06:21:40 +0100

    Ajout du projet 03

---
+++ projects/03-mini-calendar/index.js
@@ -0,0 +1,15 @@
+const monthNameEl = document.getElementById("month-name") 
+const dayNameEl = document.getElementById("day-name") 
+const dayNumEl = document.getElementById("day-number") 
+const yearEl = document.getElementById("year") 
+
+const date = new Date();
+console.log(date.getDate());
+// monthNameEl.innerHTML = date.getMonth()
+monthNameEl.innerHTML = date.toLocaleDateString("en", {month:"long"});
+dayNameEl.innerHTML = date.toLocaleDateString("en", 
+    {weekday:"long"}
+)
+dayNumEl.innerHTML = date.getDate();
+
+yearEl.innerHTML = date.getFullYear();
